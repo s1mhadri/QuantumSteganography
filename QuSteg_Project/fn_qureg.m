251af24b0cb28c9b08456adc38047758703a0ca2
Simhadri Holagundhi <38690426+simhadri-hs@users.noreply.github.com>
2021-05-08 01:14:52 +0200

    Add files via upload

--- QuSteg_Project/fn_qureg.m
+++ QuSteg_Project/fn_qureg.m
@@ -1,4 +1,12 @@
-function [kp] = fn_qureg(N, q)
-    kp = sparse(N+1, 1, 1, 2^q, 1);
-end
+%{
+Function to represent decimal value as a qubit register
+Input:
+N - The decimal value to be represented in quantum register form
+q - number of bits needed to represent the decimal value
+Output:
+qr - quantum register of q bits
+%}
 
+function [qr] = fn_qureg(N, q)
+    qr = sparse(N+1, 1, 1, 2^q, 1);
+end
